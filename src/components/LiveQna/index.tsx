1e6b7b89eacbe25db97be5f3ce21faf76f7ce17d
Agushm <mr.agus.him@gmail.com>
2024-08-18 11:32:22 +0700

    Add dialog in list live QnA

--- src/components/LiveQna/index.tsx
+++ src/components/LiveQna/index.tsx
@@ -31,10 +31,53 @@ const LiveQna = () => {
     }
   }, []);
 
-  const [selected, setSelected] = useState<Comment>();
-
   const handleClick = (value: Comment) => {
-    setSelected(value);
+    setDialogContent(
+      <div className="flex flex-1 items-center justify-between">
+        <div>
+          <div className="flex flex-row items-center">
+            <div
+              className={`sm:flex items-center text-center justify-center w-18 h-18 rounded-full hidden ${
+                value.user?.gender == "female" ? "bg-meta-7" : "bg-primary"
+              }`}
+              style={{ boxShadow: "0px 5px 0px 0px #000000" }}
+            >
+              <h1 className="text-white text-2xl">
+                {value.user.username?.substring(0, 2)}
+              </h1>
+            </div>
+            <h5 className="mx-3 font-bold text-white dark:text-white text-2xl">
+              {value.user.username}
+            </h5>
+          </div>
+          <p className="my-3">
+            <span className={`text-white dark:text-white text-3xl font-normal`}>
+              {value.comment}
+            </span>
+          </p>
+          <p className="text-xl mt-2">
+            {formatStrToDateTime(value.created_at!, "dd-MM-yyyy HH:mm", true)}
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center ml-5">
+          <svg
+            className="fill-danger"
+            fill="none"
+            width="50"
+            height="50"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill=""
+              d="M20.808,11.079C19.829,16.132,12,20.5,12,20.5s-7.829-4.368-8.808-9.421C2.227,6.1,5.066,3.5,8,3.5a4.444,4.444,0,0,1,4,2,4.444,4.444,0,0,1,4-2C18.934,3.5,21.773,6.1,20.808,11.079Z"
+            />
+          </svg>
+          <span className="mt-1 text-xl text-white">{value.like}</span>
+        </div>
+      </div>
+    );
+    toggleDialog();
   };
 
   if (isLoading) {
@@ -54,11 +97,7 @@ const LiveQna = () => {
         comments?.map((comment, key) => (
           <div
             onClick={() => handleClick(comment)}
-            className={`flex items-center gap-5 py-3 px-3 md:py-3 md:px-7.5 border mb-4 rounded-md ${
-              comment === selected
-                ? "border-primary border-4"
-                : "border-2 border-black"
-            }`}
+            className={`flex items-center gap-5 py-3 px-3 md:py-3 md:px-7.5 border mb-4 rounded-md border-black`}
             style={{ boxShadow: "0px 5px 0px 0px #000000" }}
             key={key}
           >
@@ -89,7 +128,11 @@ const LiveQna = () => {
                   </span>
                 </p>
                 <p className="text-md mt-2">
-                  {formatStrToDateTime(comment.created_at!, "dd-MM-yyyy HH:mm",true)}
+                  {formatStrToDateTime(
+                    comment.created_at!,
+                    "dd-MM-yyyy HH:mm",
+                    true
+                  )}
                 </p>
               </div>
               <div className="flex flex-col items-center justify-center">
@@ -106,7 +149,7 @@ const LiveQna = () => {
                     d="M20.808,11.079C19.829,16.132,12,20.5,12,20.5s-7.829-4.368-8.808-9.421C2.227,6.1,5.066,3.5,8,3.5a4.444,4.444,0,0,1,4,2,4.444,4.444,0,0,1,4-2C18.934,3.5,21.773,6.1,20.808,11.079Z"
                   />
                 </svg>
-                <span className="mt-1 text-md md:text-xl">{comment.like}</span>
+                <span className="mt-1 text-md md:text-xl text-white">{comment.like}</span>
               </div>
             </div>
           </div>
--- src/components/common/Dialog/DialogShowQnA.tsx
+++ src/components/common/Dialog/DialogShowQnA.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { forwardRef } from "react";
 
 type Props = {
@@ -15,14 +16,31 @@ const DialogShowQnA = forwardRef<HTMLDialogElement, Props>(
             toggleDialog();
           }
         }}
-        className="modal"
+        className="modal bg-meta-7"
       >
-        <div
-          className="modal-box bg-white dark:bg-boxdark-2 border-2 border-black w-full"
-          style={{ boxShadow: "8px 8px 0px #000000" }}
-        >
-           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={toggleDialog}>✕</button>
-          {children}
+        <div className="flex flex-col items-center">
+          <div
+            className="modal-box bg-boxdark-2 border-2 border-black w-11/12 max-w-5xl p-10"
+            style={{ boxShadow: "8px 8px 0px #000000" }}
+          >
+            <button
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={toggleDialog}
+            >
+              ✕
+            </button>
+            {children}
+          </div>
+
+          <div className="flex flex-row mt-15 items-center">
+            <Image
+              width={70}
+              height={70}
+              src={"/images/logo/yn_logo.png"}
+              alt="Logo"
+            />
+            <h1 className="ml-3 text-[#3C4DDC] font-medium text-3xl">YukNgaji Solo</h1>
+          </div>
         </div>
       </dialog>
     );
