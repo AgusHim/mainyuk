344a8e575cec8083e20b246446c2cfa19b7b2c51
Agushm <mr.agus.him@gmail.com>
2024-10-05 09:01:28 +0700

    Add page redirect page DaftarKey13

---
+++ src/app/DaftarKey13/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+
+export default function DaftarKey13Page() {
+  redirect('/events/key13');  // Or an absolute URL: 'http://example.com/url2'
+  return null;
+}
