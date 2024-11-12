278f04ae08f5a945b92fb094ea94ed87ec1bd3d0
Agushm <mr.agus.him@gmail.com>
2024-11-12 16:13:47 +0700

    Fix issue npm build

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,12 +1,15 @@
+"use client"
 import dynamic from "next/dynamic";
-const SignOTPPage = dynamic(() => import("@/components/Pages/SignOTPPage"));
-import { Metadata } from "next";
+const SignOTPPage = dynamic(() => import("@/components/Pages/SignOTPPage"),{
+  ssr: false,
+});
 
-export const metadata: Metadata = {
-  title: "Masuk - YukNgaji Solo",
-  description: "Halaman signin YukNgaji Solo",
-  // other metadata
-};
+// import { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "Masuk - YukNgaji Solo",
+//   description: "Halaman signin YukNgaji Solo",
+//   // other metadata
+// };
 
 const SignIn: React.FC = () => {
   return (
