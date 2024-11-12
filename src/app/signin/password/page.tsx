bdea672d0e82aeed3f992849307a95ad13d84a2f
Agushm <mr.agus.him@gmail.com>
2024-11-12 15:59:04 +0700

    Fix issue npm build

--- src/app/signin/password/page.tsx
+++ src/app/signin/password/page.tsx
@@ -1,12 +1,14 @@
+"use client"
 import dynamic from "next/dynamic";
-const SignInPage = dynamic(() => import("@/components/Pages/SignInPage"));
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Masuk - YukNgaji Solo",
-  description: "Halaman signin YukNgaji Solo",
-  // other metadata
-};
+const SignInPage = dynamic(() => import("@/components/Pages/SignInPage"), {
+  ssr: false,
+});
+// import { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "Masuk - YukNgaji Solo",
+//   description: "Halaman signin YukNgaji Solo",
+//   // other metadata
+// };
 
 const SignIn: React.FC = () => {
   return (
